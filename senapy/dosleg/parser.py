80b533b98aacd4c5de920a5f57c3472e0b7df61e
Damien <damien@dam.io>
2017-10-25 14:42:15 +0200

    there was a multi-depot regression, add test case

--- senapy/dosleg/parser.py
+++ senapy/dosleg/parser.py
@@ -334,8 +334,8 @@ def parse(html, url_senat=None, logfile=sys.stderr):
                 if url:
                     step['source_url'] = clean_url(url)
 
-            # there can be multiple texts inside an hemicycle step, ok for CMP but not ok for other steps
-            if len(steps_to_add) > 1 and step.get('stage') != 'CMP':
+            # there can be multiple texts inside an hemicycle step, ok for CMP and multi-depots but not ok for other steps
+            if len(steps_to_add) > 1 and step.get('stage') != 'CMP' and step.get('step') != 'depot':
                 log_error('MULTIPLE TEXTS BUT NOT CMP.hemicycle - %s.%s.%s' % (step['institution'], step.get('stage'), step.get('step')))
                 steps_to_add = [steps_to_add[-1]]
 
